22105a23b3410efbae2e4aab529a9f42d90ec890
Layan-Alghamdi <162414697+Layan-Alghamdi@users.noreply.github.com>
2024-11-06 14:33:27 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+import numpy as np
+import pandas as pd
+import streamlit as st
+
+st.title("This is first Assignment")
+st.write("This assignment is for cloud computing")
+
+data = pd.DataFrame({
+    "C1": ['A', 'B', 'C', 'D', 'E'],
+    "C2": [100, 200, 300, 400, 500]
+})
+
+st.write("This is simple data")
+st.write(data)
+
+chart_data = pd.DataFrame(
+    np.random.randn(20, 4),
+    columns=['P', 'Q', 'R', 'T']
+)
+
+st.line_chart(chart_data)
+
+
+
